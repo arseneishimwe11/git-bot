934e7cf0f2b8826f86aacd219c93ea668357a1c6
arseneishimwe11 <arsenuga11@gmail.com>
2023-06-24 07:37:25 +0200

    2023-06-24T07:37:25+02:00

--- index.js
+++ index.js
@@ -1,22 +1,34 @@
 const jsonfile = require('jsonfile');
-const FILE_PATH = './data.json'
+const FILE_PATH = './data.json';
 const moment = require('moment');
-const random = require('random')
-const simpleGit = require('simple-git')
+const simpleGit = require('simple-git');
 
-const makeCommit  = n => {
-    const x = random.int(0, 54); 
-    const y     = random.int(0, 6)
+// Function to generate a random number between min and max
+const getRandomInt = (min, max) => {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+const makeCommit = n => {
+    if (n <= 0) {
+        return;
+    }
+
+    const x = getRandomInt(0, 54);
+    const y = getRandomInt(0, 6);
     const DATE = moment().subtract(1, 'y').add(1, 'd')
-        add(x, 'w').add(y, 'd').format()
-    data =  {
+        .add(x, 'w').add(y, 'd').format();
+    const data = {
         date: DATE
     }
-    console.log(DATE);
-    jsonfile.writeFile(FILE_PATH , data, ()=>{
-        simpleGit().add([FILE_PATH]).commit(DATE, {'--date' : DATE}).push();
-        makeCommit.bind(this, --n)
 
+    console.log(`Committing with date: ${DATE}`);
+
+    jsonfile.writeFile(FILE_PATH, data, () => {
+        simpleGit().add([FILE_PATH]).commit(DATE, { '--date': DATE }).push(() => {
+            makeCommit(n - 1);
+        });
     });
 }
-makeCommit(120)
+
+const numberOfCommits = 120; 
+makeCommit(numberOfCommits);
